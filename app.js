6e520691a6ba38fad9425727af77b68f9ccb6960
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-07-09 12:36:09 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const ExpressError = require("./utils/ExpressError");
+
+const dbUrl =
+  process.env.DB_URL || "mongodb://localhost:27017/hotel-reservation-app";
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/hotels", (req, res) => {
+  res.status(200).json({ test: "asdfasf" });
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((error, req, res, next) => {
+  const { status = 500, message = "Something went wrong." } = error;
+  res.status(status).send({ message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
